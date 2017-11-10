873e87134fc1db6f27e3b3b3e1c21c651da10412
Angelo Barcelo <angelobarcelo@hotmail.com>
2017-11-10 17:21:44 -0500

    Added two teams

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,39 @@
-/**
- * Displays the given score for Team A.
- */
-public void displayForTeamA(int score) {
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int currentScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
-        }
+    }
+
+    public void twoPoints(View v) {
+        currentScore += 2;
+        displayForTeamA(currentScore);
+    }
+
+    public void threePoints(View v) {
+        currentScore += 3;
+        displayForTeamA(currentScore);
+    }
+
+    public void onePoint(View v) {
+        currentScore ++;
+        displayForTeamA(currentScore);
+    }
+}
